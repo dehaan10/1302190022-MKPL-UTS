f917c14dbb41d29da67a2b7d2ef95efc75ba1dc0
dehaan10 <102848392+dehaan10@users.noreply.github.com>
2022-04-20 22:30:53 +0700

    Membuat beberapa file class baru
    Menghapus beberapa variable pada Employee dan memindahkannya ke file class baru yaitu Pribadi dan Pernikahan

---
+++ Pribadi.java
@@ -0,0 +1,41 @@
+package lib;
+
+public class Pribadi {
+
+    private String firstName;
+	private String lastName;
+	private String idNumber;
+	private String address;
+}
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getIdNumber() {
+        return this.idNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
